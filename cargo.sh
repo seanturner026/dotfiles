ce04f1350f30a6c93b62c15474ad08c4c9a7077b
Sean Turner <sean.turner026@gmail.com>
2024-02-14 21:02:31 -0500

    Updated dotfiles

--- cargo.sh
+++ cargo.sh
@@ -2,3 +2,4 @@ cargo install --locked zellij # https://zellij.dev/documentation/installation
 cargo install fd-find # https://github.com/sharkdp/fd#installation
 cargo install stylua # https://github.com/JohnnyMorganz/StyLua
 cargo install vscli # https://github.com/michidk/vscli
+cargo install zoxide # https://github.com/ajeetdsouza/zoxide
