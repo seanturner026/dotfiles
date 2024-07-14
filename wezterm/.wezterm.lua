e5096925845433cc588760004af380edb7f2d1e9
Sean Turner <sean.turner026@gmail.com>
2024-07-14 13:06:37 -0400

    Wezterm padding

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -5,9 +5,14 @@ return {
   font_size = 16.0,
   font = wezterm.font("JetBrains Mono"),
   macos_window_background_blur = 30,
-
   window_background_opacity = 1.0,
   window_decorations = "RESIZE",
+  window_padding = {
+    left = 0,
+    -- top = 0,
+    right = 0,
+    bottom = 0,
+  },
   keys = {
     {
       key = "f",
