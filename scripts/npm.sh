76ff35dafb17c7792e7e067d9b4f06fe6f978363
Sean Turner <sean.turner026@gmail.com>
2024-08-19 17:29:28 -0400

    Adding devcontainer cli

--- scripts/npm.sh
+++ scripts/npm.sh
@@ -1,3 +1,4 @@
-sudo npm install --global @mhlabs/evb-cli # https://github.com/mhlabs/evb-cli
-sudo npm install --global pnpm            # https://pnpm.io/installation
-sudo npm install --global yarn            # https://classic.yarnpkg.com/lang/en/docs/install/#mac-stable
+sudo npm install --global @devcontainers/cli # https://github.com/devcontainers/cli
+sudo npm install --global @mhlabs/evb-cli    # https://github.com/mhlabs/evb-cli
+sudo npm install --global pnpm               # https://pnpm.io/installation
+sudo npm install --global yarn               # https://classic.yarnpkg.com/lang/en/docs/install/#mac-stable
