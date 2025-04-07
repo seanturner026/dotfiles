50f9ea81b395aa982d4b30fbfb2612f6818c610a
Sean Turner <sean.turner026@gmail.com>
2025-04-07 06:07:47 -0400

    Added harper

--- scripts/cargo.sh
+++ scripts/cargo.sh
@@ -3,6 +3,7 @@ cargo install --locked zellij # https://zellij.dev/documentation/installation
 cargo install difftastic # https://github.com/wilfred/difftastic
 cargo install fd-find # https://github.com/sharkdp/fd#installation
 cargo install git-delta # https://github.com/dandavison/delta
+cargo install harper-ls --locked # https://github.com/Automattic/harper
 cargo install stylua # https://github.com/JohnnyMorganz/StyLua
 cargo install uv # https://github.com/astral-sh/uv
 cargo install vscli # https://github.com/michidk/vscli
