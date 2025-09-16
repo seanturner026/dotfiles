90356e48f52c029e695f82a259f269ff47aed83f
Sean Turner <sean.turner026@gmail.com>
2025-09-16 14:57:46 -0500

    Adding newline for CC

--- wezterm/keys.lua
+++ wezterm/keys.lua
@@ -4,26 +4,30 @@ local action = wezterm.action
 local function keys(config)
   config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 10000 }
   config.keys = {
-    { key = "f",       mods = "CTRL",      action = action.ToggleFullScreen },
+
+    { key = "f", mods = "CTRL", action = action.ToggleFullScreen },
+
+    -- Claude Code newline
+    { key = "Enter", mods = "SHIFT", action = wezterm.action.SendString("\n") },
 
     -- Navigate words like iterm
-    { mods = "OPT",    key = "LeftArrow",  action = action.SendKey({ mods = "ALT", key = "b" }) },
-    { mods = "OPT",    key = "RightArrow", action = action.SendKey({ mods = "ALT", key = "f" }) },
-    { mods = "CMD",    key = "LeftArrow",  action = action.SendKey({ mods = "CTRL", key = "a" }) },
-    { mods = "CMD",    key = "RightArrow", action = action.SendKey({ mods = "CTRL", key = "e" }) },
-    { mods = "CMD",    key = "Backspace",  action = action.SendKey({ mods = "CTRL", key = "u" }) },
+    { mods = "OPT", key = "LeftArrow", action = action.SendKey({ mods = "ALT", key = "b" }) },
+    { mods = "OPT", key = "RightArrow", action = action.SendKey({ mods = "ALT", key = "f" }) },
+    { mods = "CMD", key = "LeftArrow", action = action.SendKey({ mods = "CTRL", key = "a" }) },
+    { mods = "CMD", key = "RightArrow", action = action.SendKey({ mods = "CTRL", key = "e" }) },
+    { mods = "CMD", key = "Backspace", action = action.SendKey({ mods = "CTRL", key = "u" }) },
 
     -- tmux-like bindings
-    { mods = "LEADER", key = "c",          action = action.SpawnTab("CurrentPaneDomain") },
-    { mods = "LEADER", key = "x",          action = action.CloseCurrentPane({ confirm = true }) },
-    { mods = "LEADER", key = "b",          action = action.ActivateTabRelative(-1) },
-    { mods = "LEADER", key = "n",          action = action.ActivateTabRelative(1) },
-    { mods = "LEADER", key = "v",          action = action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
-    { mods = "LEADER", key = "s",          action = action.SplitVertical({ domain = "CurrentPaneDomain" }) },
-    { mods = "OPT",    key = "h",          action = action.ActivatePaneDirection("Left") },
-    { mods = "OPT",    key = "j",          action = action.ActivatePaneDirection("Down") },
-    { mods = "OPT",    key = "k",          action = action.ActivatePaneDirection("Up") },
-    { mods = "OPT",    key = "l",          action = action.ActivatePaneDirection("Right") },
+    { mods = "LEADER", key = "c", action = action.SpawnTab("CurrentPaneDomain") },
+    { mods = "LEADER", key = "x", action = action.CloseCurrentPane({ confirm = true }) },
+    { mods = "LEADER", key = "b", action = action.ActivateTabRelative(-1) },
+    { mods = "LEADER", key = "n", action = action.ActivateTabRelative(1) },
+    { mods = "LEADER", key = "v", action = action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+    { mods = "LEADER", key = "s", action = action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+    { mods = "OPT", key = "h", action = action.ActivatePaneDirection("Left") },
+    { mods = "OPT", key = "j", action = action.ActivatePaneDirection("Down") },
+    { mods = "OPT", key = "k", action = action.ActivatePaneDirection("Up") },
+    { mods = "OPT", key = "l", action = action.ActivatePaneDirection("Right") },
 
     -- Reside panes without hitting leader multiple times
     {
@@ -40,11 +44,11 @@ local function keys(config)
 
   config.key_tables = {
     resize_pane = {
-      { key = "h",      action = action.AdjustPaneSize({ "Left", 5 }) },
-      { key = "j",      action = action.AdjustPaneSize({ "Down", 5 }) },
-      { key = "k",      action = action.AdjustPaneSize({ "Up", 5 }) },
-      { key = "l",      action = action.AdjustPaneSize({ "Right", 5 }) },
-      { key = "Enter",  action = "PopKeyTable" },
+      { key = "h", action = action.AdjustPaneSize({ "Left", 5 }) },
+      { key = "j", action = action.AdjustPaneSize({ "Down", 5 }) },
+      { key = "k", action = action.AdjustPaneSize({ "Up", 5 }) },
+      { key = "l", action = action.AdjustPaneSize({ "Right", 5 }) },
+      { key = "Enter", action = "PopKeyTable" },
       { key = "Escape", action = "PopKeyTable" },
     },
   }
