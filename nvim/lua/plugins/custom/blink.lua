d93ed9efe8f7207a055c0fe3bd40d44ec18acc29
Sean Turner <sean.turner026@gmail.com>
2026-01-26 20:43:53 -0500

    Removing incorrect configuration

--- nvim/lua/plugins/custom/blink.lua
+++ nvim/lua/plugins/custom/blink.lua
@@ -16,6 +16,5 @@ return {
         },
         sources = { default = { "lsp", "path", "snippets", "buffer" } },
         fuzzy = { implementation = "prefer_rust_with_warning" },
-        opts_extend = { "sources.default" },
     },
 }
