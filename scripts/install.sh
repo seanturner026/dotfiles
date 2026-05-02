89d98ec8f23240016c62fd3913154d157a4a7fae
Sean Turner <sean.turner026@gmail.com>
2026-05-02 14:25:33 -0400

    Adding atlas

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,4 @@
+curl -sSf https://atlasgo.sh | sh # atlas
 curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh # atuin
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh # rustup
 curl -fsSL https://claude.ai/install.sh | bash # claude code
