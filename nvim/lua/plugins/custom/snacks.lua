b309268baa5be9c8d3be7f662772928862340a57
Sean Turner <30396892+seanturner026@users.noreply.github.com>
2026-05-02 23:54:24 -0700

    Add frecency to snacks picker and git log keymap (#30)
    Use Snacks.picker.smart() for <leader>ff with frecency + cwd bonus,
    and bind <leader>gl to Snacks.picker.git_log().

--- nvim/lua/plugins/custom/snacks.lua
+++ nvim/lua/plugins/custom/snacks.lua
@@ -3,13 +3,25 @@ return {
     priority = 1000,
     lazy = false,
     opts = {
-        picker = { enabled = true, ui_select = true },
+        picker = {
+            enabled = true,
+            ui_select = true,
+            sources = {
+                smart = {
+                    multi = { "buffers", "recent", "files" },
+                    matcher = { frecency = true, cwd_bonus = true },
+                },
+                files = {
+                    matcher = { frecency = true, cwd_bonus = true },
+                },
+            },
+        },
     },
     keys = {
         {
             "<leader>ff",
             function()
-                Snacks.picker.files()
+                Snacks.picker.smart()
             end,
             desc = "[F]ind [F]iles",
         },
@@ -26,6 +38,13 @@ return {
                 Snacks.picker.buffers()
             end,
             desc = "Find existing buffers",
+        },
+        {
+            "<leader>gl",
+            function()
+                Snacks.picker.git_log()
+            end,
+            desc = "[G]it [L]og",
         },
     },
 }
